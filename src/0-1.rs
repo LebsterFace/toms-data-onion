1141f05d85720ccf5ecda39b583ad73acd823a93
LebsterFace <samsalinsky@gmail.com>
2026-07-06 02:55:00 +0100

    Layer 0 in Rust
    Co-authored-by: Eva Porator <EVAPorator-sudo@users.noreply.github.com>

---
+++ src/0-1.rs
@@ -0,0 +1,71 @@
+use std::fs;
+use std::str;
+
+pub const MARKER: &str = "==[ Payload ]===============================================\n\n";
+
+/**
+ * Reads the next stage's payload from the data onion
+ */
+fn read() -> String {
+    let mut onion = fs::read_to_string("input.txt").unwrap();
+    let index = onion.find(MARKER).unwrap() + MARKER.len();
+    onion
+        .split_off(index)
+        .replace("\n", "")
+        .replace(" ", "")
+        .replace("\t", "")
+        .to_owned()
+}
+
+/**
+ * Pads an array of u8 with 'u' bytes until the length == 5
+ * Returns the padded chunk and the number of bytes added
+ */
+fn pad_chunk(chunk: &[u8]) -> ([u8; 5], usize) {
+    let mut result: [u8; 5] = [b'u'; 5];
+    let amount_to_pad = 5 - chunk.len();
+    for i in 0..chunk.len() {
+        result[i] = chunk[i];
+    }
+
+    (result, amount_to_pad)
+}
+
+/**
+ * Converts the raw Ascii85 input into the chunks
+ * Automatically applies padding to the final chunk
+ * Returns the chunks and the number of 'u' bytes added as padding
+ */
+fn split_to_chunks(input: String) -> (Vec<[u8; 5]>, usize) {
+    let expanded = input.replace("z", "!!!!!");
+    let data = expanded
+        .strip_prefix("<~")
+        .unwrap()
+        .strip_suffix("~>")
+        .unwrap();
+    let (chunks, last): (&[[u8; 5]], &[u8]) = data.as_bytes().as_chunks::<5>();
+    let mut result = chunks.to_vec();
+    let (padded, padding_characters) = pad_chunk(last);
+    result.push(padded);
+    (result, padding_characters)
+}
+
+fn ascii85(input: String) -> String{
+    let (chunks, padding) = split_to_chunks(input);
+    let decoded_chunks: Vec<String> = chunks.iter().map(|chunk| {
+        let large = ((chunk[0] - b'!') as u32) * (85_u32).pow(4) +
+                    ((chunk[1] - b'!') as u32) * (85_u32).pow(3) +
+                    ((chunk[2] - b'!') as u32) * (85_u32).pow(2) +
+                    ((chunk[3] - b'!') as u32) * (85_u32).pow(1) +
+                    ((chunk[4] - b'!') as u32) * (85_u32).pow(0);
+        String::from_utf8(large.to_be_bytes().to_vec()).unwrap()
+    }).collect();
+
+    let result = decoded_chunks.join("");
+    return String::from(&result[..(result.len() - padding)]);
+}
+
+fn main() {
+    let data = read();
+    fs::write("./output.txt", ascii85(data));
+}
