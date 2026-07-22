73a1ddd4c15e5bf509c05241c2516d1c6953b63d
LebsterFace <samsalinsky@gmail.com>
2026-07-22 19:22:00 +0100

    Layer 1 in Java
    Co-authored-by: Eva Porator <EVAPorator-sudo@users.noreply.github.com>

---
+++ src/1-2.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.function.IntBinaryOperator;
+
+String readRawData() {
+	try {
+		final String result = Files.readString(Path.of("parts/1.txt"));
+		return result
+				.replaceAll("\r?\n", "")
+				.split("<~|~>")[1];
+	} catch (IOException e) {
+		throw new Error(e);
+	}
+}
+
+byte[] ascii85(String input) {
+	input = input.replace("z", "!!!!!");
+	final int padding = input.length() % 5;
+	input += "u".repeat(padding);
+
+	final byte[] bytes = new byte[(input.length() * 4) / 5];
+	int byte_count = 0;
+	for (int i = 0; i < input.length() - 5; i += 5) {
+		final String chunk = input.substring(i, i + 5);
+		final char[] chars = chunk.toCharArray();
+		long result = ((long) (chars[0] - '!')) * Math.powExact(85, 4) +
+				      ((long) (chars[1] - '!')) * Math.powExact(85, 3) +
+				      ((long) (chars[2] - '!')) * Math.powExact(85, 2) +
+				      ((long) (chars[3] - '!')) * Math.powExact(85, 1) +
+				      ((long) (chars[4] - '!')) * Math.powExact(85, 0);
+		bytes[byte_count++] = (byte) ((result & 0xFF000000L) >>> (8 * 3));
+		bytes[byte_count++] = (byte) ((result & 0x00FF0000L) >>> (8 * 2));
+		bytes[byte_count++] = (byte) ((result & 0x0000FF00L) >>> (8 * 1));
+		bytes[byte_count++] = (byte) ((result & 0x000000FFL) >>> (8 * 0));
+	}
+
+	return bytes;
+}
+
+
+int process(byte input) {
+	int result = input;
+	// 1. Flip every second bit
+	result = (result ^ 0b01010101);
+
+	// 2. Rotate the bits one position to the right
+	int lsb = result & 1;
+	lsb <<= 7;
+	result = (result >>> 1);
+	result = (result & 0b01111111) | lsb;
+	result = result | lsb;
+	return result;
+}
+
+
+void main() throws IOException {
+	final byte[] input = ascii85(readRawData());
+
+	StringBuilder output = new StringBuilder(input.length);
+
+	for (byte b : input) {
+		output.append((char) process(b));
+	}
+
+	output.trimToSize();
+	Files.writeString(Path.of("parts/2.txt"), output.toString(), Charset.forName("utf8"));
+}
