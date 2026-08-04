f8cc8193ee8e94cea95f2dbeae49c55d15ed2b2b
LebsterFace <samsalinsky@gmail.com>
2026-08-04 22:15:02 +0100

    Fix Ascii85 decoding layer 0

--- src/layer0.rs
+++ src/layer0.rs
@@ -1,16 +1,17 @@
 use std::fs;
 use std::str;
 
-pub const MARKER: &str = "==[ Payload ]===============================================\n\n";
+pub const MARKER: &str = "==[ Payload ]===============================================";
 
 /**
  * Reads the next stage's payload from the data onion
  */
 fn read() -> String {
-    let mut onion = fs::read_to_string("input.txt").unwrap();
+    let mut onion = fs::read_to_string("./parts/0.txt").unwrap();
     let index = onion.find(MARKER).unwrap() + MARKER.len();
     onion
         .split_off(index)
+        .replace("\r", "")
         .replace("\n", "")
         .replace(" ", "")
         .replace("\t", "")
@@ -43,29 +44,38 @@ fn split_to_chunks(input: String) -> (Vec<[u8; 5]>, usize) {
         .unwrap()
         .strip_suffix("~>")
         .unwrap();
-    let (chunks, last): (&[[u8; 5]], &[u8]) = data.as_bytes().as_chunks::<5>();
+    let (chunks, last) = data.as_bytes().as_chunks::<5>();
     let mut result = chunks.to_vec();
+
+    // no padding needed; already multiple of 5
+    if last.is_empty() {
+        return (result, 0);
+    }
+
     let (padded, padding_characters) = pad_chunk(last);
     result.push(padded);
     (result, padding_characters)
 }
 
-fn ascii85(input: String) -> String{
+fn ascii85(input: String) -> Vec<u8> {
     let (chunks, padding) = split_to_chunks(input);
-    let decoded_chunks: Vec<String> = chunks.iter().map(|chunk| {
-        let large = ((chunk[0] - b'!') as u32) * (85_u32).pow(4) +
-                    ((chunk[1] - b'!') as u32) * (85_u32).pow(3) +
-                    ((chunk[2] - b'!') as u32) * (85_u32).pow(2) +
-                    ((chunk[3] - b'!') as u32) * (85_u32).pow(1) +
-                    ((chunk[4] - b'!') as u32) * (85_u32).pow(0);
-        String::from_utf8(large.to_be_bytes().to_vec()).unwrap()
-    }).collect();
+    let mut result: Vec<u8> = Vec::with_capacity(chunks.len() * 4);
 
-    let result = decoded_chunks.join("");
-    return String::from(&result[..(result.len() - padding)]);
+    for chunk in chunks.iter() {
+        let large = ((chunk[0] - b'!') as u32) * (85_u32).pow(4) + 
+                    ((chunk[1] - b'!') as u32) * (85_u32).pow(3) + 
+                    ((chunk[2] - b'!') as u32) * (85_u32).pow(2) + 
+                    ((chunk[3] - b'!') as u32) * (85_u32).pow(1) + 
+                    ((chunk[4] - b'!') as u32) * (85_u32).pow(0);
+        result.extend_from_slice(&large.to_be_bytes());
+    }
+
+    // remove padding
+    result.truncate(result.len() - padding);
+    result
 }
 
 fn main() {
     let data = read();
-    fs::write("./output.txt", ascii85(data));
+    fs::write("./parts/1.txt", ascii85(data)).unwrap();
 }
